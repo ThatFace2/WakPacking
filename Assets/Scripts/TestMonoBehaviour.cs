ce6fcb08622f61115c29a3919d622ba59ceb625c
hyeongjun <whow1101@naver.com>
2023-07-01 18:08:10 +0900

    chore: editorconfig 추가 - 코드 스타일 및 컨벤션 추가

---
+++ Assets/Scripts/TestMonoBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // 정적인 값을 가지는 public field
+    // 즉, serialize field, const 등 값이 바뀌지 않는 다는 것을 기대할 수 있는 field
+    #region Public Field
+    #endregion
+
+    #region Protected Field
+    #endregion
+
+    #region Private Field
+    #endregion
+
+    // private field 중 setter와 getter를 명시하는 property
+    // setter와 getter가 하는 일이 많아지게 된다면(side effect가 크다면) public method를 사용하는 것을 고려
+    #region Public Property
+    #endregion
+
+    #region Unity Event
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
